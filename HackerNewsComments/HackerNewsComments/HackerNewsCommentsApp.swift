b8f6361b34cc993039c31283e1cc4d1567b0324c
Benjamin Lewis <23inhouse@gmail.com>
2022-06-20 15:51:21 +0200

    Add boiler plate files

---
+++ HackerNewsComments/HackerNewsComments/HackerNewsCommentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsCommentsApp.swift
+//  HackerNewsComments
+//
+//  Created by Benjamin Lewis on 20/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsCommentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
